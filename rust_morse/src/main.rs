07d88ce4c5f37f4f104664b65adf1f00830600ec
= <=>
2023-11-03 15:48:08 +0100

    Actual working example of a blinking led

--- rust_morse/src/main.rs
+++ rust_morse/src/main.rs
@@ -3,6 +3,7 @@ use std::thread;
 use std::time::Duration;
 
 use rppal::gpio::Gpio;
+use rppal::gpio::OutputPin;
 use rppal::system::DeviceInfo;
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
@@ -11,15 +12,13 @@ const GPIO_LED: u8 = 23;
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
 
-    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+    let mut pin: OutputPin = Gpio::new()?.get(GPIO_LED)?.into_output();
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
-    while True {
+    loop {
         pin.set_high();
         thread::sleep(Duration::from_millis(500));
         pin.set_low();
         thread::sleep(Duration::from_millis(500));
     }
-
-    Ok(())
 }
